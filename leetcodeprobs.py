4b1c0fbf231018bee102df33315ee753e99becca
Harsh Parekh <harshparekh0709@gmail.com>
2025-10-22 19:49:26 +0530

    Leet-code Day-1

---
+++ leetcodeprobs.py
@@ -0,0 +1,12 @@
+# Day 1 - TWO SUM
+class Solution:
+    def twoSum(self, nums, target):
+        # Loop through each number (first number)
+        for i in range(len(nums)):
+            # Loop through each number again (second number)
+            for j in range(i + 1, len(nums)):
+                # Check if both numbers add up to target
+                if nums[i] + nums[j] == target:
+                    return [i, j]  # return their positions
+
+
